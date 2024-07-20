636c4fa5447f73b283b9765cdb11d075bddd7299
Fernando Halmenschlager Filter <fernando.filter@universo.univates.br>
2024-07-20 11:03:27 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -3,5 +3,5 @@ import app from './app'
 const port = process.env.SERVER_PORT ?? 8080
 
 app.listen(port, () => {
-  console.log(`Listening at port ${port}!!!`)
+  console.log(`Listening at port ${port}!`)
 })
