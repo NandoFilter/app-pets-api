2a66c48924498821e827e3961e3e608b7a2c7534
Fernando Halmenschlager Filter <fernando.filter@universo.univates.br>
2024-07-22 18:31:46 -0300

    Created table

---
+++ src/database/migrations/0002_create_categoria.ts
@@ -0,0 +1,13 @@
+import { Knex } from 'knex'
+
+export async function up(knex: Knex) {
+  return knex.schema
+    .createTable('categoria', (table) => {
+      table.bigIncrements('id').primary().index()
+      table.string('descricao', 150).index().notNullable()
+    })
+}
+
+export async function down(knex: Knex) {
+  return knex.schema.dropTable('categoria')
+}
